61f71d5b02986118412fa02578610f07ed6b22ff
Manjusha Tomar <Manjusha.Tomar@ibm.com>
2024-07-04 17:47:24 +0530

    flask api has changed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 import os
 import requests
 import json
@@ -8,12 +8,12 @@ app = Flask(__name__)
 load_dotenv()
 
 BearerToken = ""
+IBM_APIKEY = os.getenv('IBM_APIKEY')
 
 dict_guidelines = {'generated_text': ['https://us-south.ml.cloud.ibm.com/ml/v1/deployments/lacajaprompt1/text/generation?version=2021-05-01']} 
 
 def load_access():
     global BearerToken
-    IBM_APIKEY = os.getenv('IBM_APIKEY')
     url = "https://iam.cloud.ibm.com/identity/token"
     payload = f'grant_type=urn%3Aibm%3Aparams%3Aoauth%3Agrant-type%3Aapikey&apikey={IBM_APIKEY}'
     headers = {
@@ -25,25 +25,25 @@ def load_access():
         print("Error: Unable to retrieve access token")
         print("Response:", response.json())
 
-@app.route('/', methods=['GET'])
-def home():
-    return jsonify({"message": "Deployed Prompt API is created"}), 200
-
-@app.route('/favicon.ico')
-def favicon():
-    return '', 204
-
 @app.route('/load_access', methods=['GET'])
 def load_access_endpoint():
     load_access()
     return jsonify({'BearerToken': BearerToken})
+
+def refresh_token_if_expired(response):
+    if response.status_code == 401:
+        response_json = response.json()
+        if 'errors' in response_json and response_json['errors'][0]['code'] == 'authentication_token_expired':
+            load_access()
+            return True
+    return False
 
 @app.route('/prompt', methods=['POST'])
 def prompt_endpoint():
     data = request.get_json()
     input_text = data['input']
     result = ""
-    
+
     for metric in dict_guidelines.keys():
         urls = dict_guidelines[metric]
         for url in urls:
@@ -60,6 +60,10 @@ def prompt_endpoint():
             }
             response = requests.post(url, headers=headers, data=payload)
             
+            if refresh_token_if_expired(response):
+                headers['Authorization'] = 'Bearer ' + BearerToken
+                response = requests.post(url, headers=headers, data=payload)
+
             # Debugging: Print the response JSON
             response_json = response.json()
             print("Response JSON from {}: {}".format(url, response_json))
@@ -69,7 +73,7 @@ def prompt_endpoint():
                 result = response_json["results"][0].get("generated_text", "No generated text")
             else:
                 result = "Error: 'results' key not found in response"
-    
+
     return jsonify({'generated_text': result})
 
 if __name__ == '__main__':
